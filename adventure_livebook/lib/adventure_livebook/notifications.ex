87d8cbd6300b6fcd9f1a92caabd66d74e09a6e05
michael <michaelgdimmitt@gmail.com>
2025-01-15 22:46:08 -0500

    initial updated to chew on tomorrow morning

--- adventure_livebook/lib/adventure_livebook/notifications.ex
+++ adventure_livebook/lib/adventure_livebook/notifications.ex
@@ -1,15 +1,16 @@
-defmodule Pgsub.Notifications do
+defmodule AdventureLivebook.Notifications do
   use GenServer
-  alias Pgsub.{Pgsub.Todo, Repo}
-  import Poison, only: [decode!: 1]
+  alias AdventureLivebook.{AdventureLivebook.Blog.Post, Repo}
+  import Poison, only: [decode!: 1] # does poison need to be added to mix.exs?
   def start_link(channel) do
     GenServer.start_link(__MODULE__, channel)
   end
+  # todos: what should todo and todos be changed to? post and posts? or blog.post and blog.posts?
   def init(channel) do
-    {:ok, pid} = Application.get_env(:pgsub, Pgsub.Repo)
+    {:ok, pid} = Application.get_env(:adventure_livebook, AdventureLivebook.Repo)
       |> Postgrex.Notifications.start_link()
     ref = Postgrex.Notifications.listen!(pid, channel)
-    data = Todo |> Repo.all
+    data = Blog.Post |> Repo.all
     {:ok, {pid, ref, channel, data}}
   end
   @topic "todo:list"
@@ -23,10 +24,10 @@ defmodule Pgsub.Notifications do
     row = for {k, v} <- raw, into: %{}, do: {String.to_atom(k), v}
     updated_data = case type do
       "UPDATE" -> Enum.map(data, fn x -> if x.id === id do Map.merge(x, row) else x end end)
-      "INSERT" -> data ++ [struct(Todo, row)]
+      "INSERT" -> data ++ [struct(Blog.Post, row)]
       "DELETE" -> Enum.filter(data,  &(&1.id !== id))
     end
-    PgsubWeb.Endpoint.broadcast!(@topic, "todos", %{todos: updated_data})
+    AdventureLivebookWeb.Endpoint.broadcast!(@topic, "todos", %{todos: updated_data})
     {:noreply, {pid, ref, channel, updated_data}}
   end
 end
